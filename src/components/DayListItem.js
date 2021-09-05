f9aaa29ddb5c8240c85b126fcdc253cc9f2a57b6
RafaySiddiqui <rafays.siddiqui@gmail.com>
2021-09-05 10:44:15 -0400

    Add conditional rendering and unit tests for 'spots remaining' in DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,17 +2,26 @@ import React from 'react';
 import 'components/DayListItem.scss';
 import classNames from 'classnames';
 
-export default  function DayListItem(props) {
+export default function DayListItem(props) {
   let dayListItemClass = classNames({
     'day-list__item': true,
     'day-list__item--selected': props.selected,
     'day-list__item--full': (props.spots === 0)
   });
+  const formatSpots = () => {
+    if (props.spots < 1) {
+      return <h3>no spots remaining</h3>;
+    } else if (props.spots === 1) {
+      return <h3>{props.spots} spot remaining</h3>;
+    } else if (props.spots > 1) {
+      return <h3 className="text--light">{props.spots} spots remaining</h3>;
+    }
+  }
 
   return (
-    <li className={dayListItemClass}  onClick={() => props.setDay(props.name)}>
+    <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} Spots Remaining</h3>
+      {formatSpots()}
     </li>
   );
 }
