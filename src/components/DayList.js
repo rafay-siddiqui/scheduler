45dad57b00b027b5c878b2b5bc66eab412ce59ab
RafaySiddiqui <rafays.siddiqui@gmail.com>
2021-09-05 12:35:39 -0400

    Add useState for selected day to change the selected day

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import DayListItem from 'components/DayListItem';
 
-export function DayList() {
+export default function DayList(props) {
+  const iterateDays = props.days.map(day => {
+    return (
+      <DayListItem 
+      key={day.id}
+      name={day.name} 
+      spots={day.spots} 
+      selected={props.day === day.name}
+      setDay={props.setDay}
+      />
+    )
+  })
+
   return (
     <ul>
-      
+      {iterateDays}
     </ul>
   );
 }
