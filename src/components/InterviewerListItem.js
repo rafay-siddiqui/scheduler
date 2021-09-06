af55d1d41da546ffa958fd4b9cdb9be1e7db9c16
RafaySiddiqui <rafays.siddiqui@gmail.com>
2021-09-06 09:18:42 -0400

    Implement basic unselected, selected and clickable stories functionality for InterviewerListItem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'components/InterviewerListItem.scss';
+import className from 'classnames';
+
+export default function InterviewerListItem(props) {
+  const interviewerItemClass = className({
+    'interviewers__item': true,
+    'interviewers__item--selected': props.selected,
+  })
+
+  return (
+    <li className={interviewerItemClass} onClick={props.setInterviewer} >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt="Sylvia Palmer"
+      />
+      {props.name}
+    </li>
+  );
+}
