1f95826852e8b8fe61bdddc93fc1907cccdf2bbb
RafaySiddiqui <rafays.siddiqui@gmail.com>
2021-09-07 05:38:45 -0400

    Create stories for Confirm component and pass props to Confirm component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from 'components/Appointment/index';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
 
 //Button Stories
 
@@ -156,6 +157,15 @@ storiesOf("Appointment", module)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
   .add("Show", () => (
     <Show
-
+      student='Lydia Miller-Jones'
+      interviewer={interviewers[0]}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
     />
   ))
+  .add("Confirm", () => (
+    <Confirm
+      message="Delete the appointment?"
+      onConfirm={action('onConfirm')}
+      onCancel={action('onCancel')}
+    />))
