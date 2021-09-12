8857bdaab1c05a97c6181bb3f7d2fe423f659c84
RafaySiddiqui <rafays.siddiqui@gmail.com>
2021-09-12 09:30:26 -0400

    Add proxy server

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 import DayList from 'components/DayList';
+import 'components/Appointment';
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -21,8 +24,61 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+  },
+  {
+    id: 4,
+    time: '10am',
+    interview: {
+      student: "Certified Donda Boy",
+      interviewer: {
+        id: 2,
+        name: "Jermaine Cole",
+        avatar: 'https://upload.wikimedia.org/wikipedia/commons/3/32/Pulitzer2018-portraits-kendrick-lamar.jpg',
+      }
+    }
+  },
+  {
+    id: 5,
+    time: '4am',
+    interview: {
+      student: "Rafay Siddiqui",
+      interviewer: {
+        id: 3,
+        name: 'Mildred Nazir',
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  }
+];
+
 export default function Application(props) {
-const [day, setDay] = useState('Monday');
+  const [day, setDay] = useState('Monday');
+  const iterateAppointments = appointments.map(appointment => {
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />)
+  })
 
   return (
     <main className="layout">
@@ -47,7 +103,10 @@ const [day, setDay] = useState('Monday');
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+
+        {iterateAppointments}
+        <Appointment key="last" time="5pm" />
+
       </section>
     </main>
   );
