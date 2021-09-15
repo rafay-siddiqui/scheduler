4bc0c88e84bccf719d1dca262d14d57884934a23
RafaySiddiqui <rafays.siddiqui@gmail.com>
2021-09-15 18:36:19 -0400

    Add custom useVisualMode hook to control Appointment View Mode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export default function useVisualMode(intial) {
+  const [mode, setMode] = useState(intial);
+  const [history, setHistory] = useState([intial]);
+
+  const editHistory = () => {
+    const array = history;
+    (history.length !== 1 && array.pop());
+    setHistory(array);
+  }
+  
+  const transition = (next, replace = false) => {
+    setMode(next)
+    if (replace) {
+      editHistory();
+    };
+    setHistory(prev => [...prev, next])
+  }
+
+  const back = () => {
+    editHistory();
+    setMode(history[history.length - 1]);
+  }
+
+  return { mode, transition, back };
+}
