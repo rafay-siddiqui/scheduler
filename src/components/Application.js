bb66b59fbcffb41cb03064b9d9bebf19734b9f7b
RafaySiddiqui <rafays.siddiqui@gmail.com>
2021-09-15 16:23:23 -0400

    Add files for useVisualMode custom hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -58,7 +58,18 @@ export default function Application(props) {
     day: 'Monday',
     days: [],
     appointments: appointmentsArr,
-    interviewers: []
+    interviewers: [{
+      "1": {
+        "id": 1,
+        "name": "Sylvia Palmer",
+        "avatar": "https://i.imgur.com/LpaY82x.png"
+      },
+      "2": {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      }
+    }]
   });
   const setDay = day => setState({ ...state, day });
   const dailyAppointments = getAppointmentsForDay(state, state.day);
@@ -69,19 +80,18 @@ export default function Application(props) {
       axios.get("http://localhost:8001/api/appointments"),
       axios.get("http://localhost:8001/api/interviewers")
     ]).then((all) => {
-      console.log('interviewers: ', all[2].data)
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
     })
-  }, [])
+  }, [state.day])
 
   const iterateAppointments = dailyAppointments.map(appointment => {
-    // const interview = getInterview(state, appointment.interview)
-    // console.log('getInterview: ', getInterview(state, appointment.interview))
-    console.log('map: ', appointment.interview)
+    const interview = getInterview(state, appointment.interview)
     return (
       <Appointment
         key={appointment.id}
-        {...appointment}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
       />)
   })
 
